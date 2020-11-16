682999badc03aa3ee8d20105475f26df62a6a563
louiscklaw <louiscklaw@gmail.com>
2020-11-16 21:50:51 +0800

    update and test,

---
+++ scripts/update_main_build_chain.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# python3 scripts/update_main_build_chain.py -u
+python3 scripts/update_main_build_chain.py
+
+git add .github/workflows/master_build.yml
+git add **-tryout/build.yml
