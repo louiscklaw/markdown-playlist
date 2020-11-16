5eddca4b22c4c6787df8b629de64e53818417829
louiscklaw <louiscklaw@gmail.com>
2020-11-16 21:45:21 +0800

    update and test,

---
+++ mermaid-helloworld-tryout/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+export PROJ_HOME=$(dirname $0)
+export PATH=$PATH:$PROJ_HOME/node_modules/.bin
+
+# mmdc -i happy_flow_1.mmd -o happy_flow_1.png
+# mmdc -i happy_flow_1.mmd -o happy_flow_1.png
+mmdc -i helloworld.mmd -o helloworld.png
