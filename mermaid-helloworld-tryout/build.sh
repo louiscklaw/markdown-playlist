eeaa3209fe3e497b12ebb5d5cc7a9f753bceb53f
louiscklaw <louiscklaw@gmail.com>
2020-11-30 20:25:59 +0800

    update readme,

--- mermaid-helloworld-tryout/build.sh
+++ mermaid-helloworld-tryout/build.sh
@@ -8,3 +8,4 @@ export PATH=$PATH:$PROJ_HOME/node_modules/.bin
 # mmdc -i happy_flow_1.mmd -o happy_flow_1.png
 # mmdc -i happy_flow_1.mmd -o happy_flow_1.png
 mmdc -i helloworld.mmd -o helloworld.png
+mmdc -i android_emulation_setup.mmd -o android_emulation_setup.png
